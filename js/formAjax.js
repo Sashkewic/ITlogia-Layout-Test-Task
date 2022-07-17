478bb63f46993068d719f50ba6875eb7607cd091
Sashkewic <sashkewicha@gmail.com>
2022-07-17 22:00:31 +0600

    добавлена анимация кнопкам в карточках пицц, подчищен ненужный код в formajax.js

--- js/formAjax.js
+++ js/formAjax.js
@@ -1,7 +1,4 @@
 let clientForm = document.querySelector('#client-form');
-let clientname = document.querySelector('#clientname').value;
-let address = document.querySelector('#address').value;
-let telephone = document.querySelector('#telephone').value;
 
 let alertBlock = document.getElementById('form-alert');
 
